1a0af8d678d0d53bbfb319182acea798f1fe65e3
Safaid Ansari <safaidansari07@gmail.com>
2023-06-09 09:12:10 +0530

    enable cors error

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
 const DB = require("./config/connection");
 const authRoute = require("./routes/auth.route");
@@ -8,6 +9,7 @@ const dashboardRoute = require("./routes/dashboard.route");
 const PORT = process.env.PORT || 3000;
 
 // Set up middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoute);
